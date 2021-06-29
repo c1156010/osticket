20778391c972d0b11edccb7ad26d66527fccb4d2
c1156010 <86585067+c1156010@users.noreply.github.com>
2021-06-29 11:53:42 +0900

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,9 +39,9 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','waws-prod-ps1-003.ftp.azurewebsites.windows.net');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
+define('DBUSER','user20osTicketsystem\$user20osTicketsystem');
 define('DBPASS','demo@pass123');
 
 # Table prefix
